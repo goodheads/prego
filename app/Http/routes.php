2e6fdd917918dc3d640f73308be6db1a6d2bb6a7
busayo <prosperotemuyiwa@gmail.com>
2015-09-21 05:49:12 +0100

    Add authentication routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,29 @@ Route::get('/', [
     'uses' => '\Prego\Http\Controllers\HomeController@index',
     'as'   => 'index'
 ]);
+
+/**
+ *  Authentication routes
+ */
+Route::get('/auth/register', [
+    'uses' => '\Tinder\Http\Controllers\AuthController@getRegister',
+    'as'   => 'auth.register',
+    'middleware' => ['guest']
+]);
+
+Route::post('/auth/register', [
+    'uses' => '\Tinder\Http\Controllers\AuthController@postRegister',
+    'middleware' => ['guest']
+]);
+
+Route::get('/auth/signin', [
+    'uses' => '\Tinder\Http\Controllers\AuthController@getLogin',
+    'as'   => 'auth.login',
+    'middleware' => ['guest']
+]);
+
+Route::post('/auth/signin', [
+    'uses' => '\Tinder\Http\Controllers\AuthController@postLogIn',
+    'middleware' => ['guest']
+]);
+
