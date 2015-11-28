19db7ec7f1c7c24329421fb7bc3b5ea2b1d57361
busayo <prosperotemuyiwa@gmail.com>
2015-11-28 09:09:52 +0300

    [Prego] Do some home cleaning and adjust session life time expiry date

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \Prego\Http\Middleware\VerifyCsrfToken::class,
+        //\Unicodeveloper\Session\Middleware\SessionTimeout::class,
     ];
 
     /**
